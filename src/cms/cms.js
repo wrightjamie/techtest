2bce62d19de63cf6c7d1b8ea481a9a269374e0c0
wrightjamie <wright_jamie@hotmail.com>
2022-04-24 10:16:56 +0100

    netlify credentials

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,10 +6,17 @@ window.CMS_MANUAL_INIT = true;
 CMS.init({
   config: {
     load_config_file: false,
-    display_url: "http://localhost:8000",
     backend: {
       name: "git-gateway",
       branch: "master",
+      repo: "techtest",
+      commit_messages: {
+        create: "Create {{collection}} “{{slug}}”",
+        update: "Update {{collection}} “{{slug}}”",
+        delete: "Delete {{collection}} “{{slug}}”",
+        uploadMedia: "[skip ci] Upload “{{path}}”",
+        deleteMedia: "[skip ci] Delete “{{path}}”",
+      },
     },
     media_folder: "/static/img",
     public_folder: "/img",
