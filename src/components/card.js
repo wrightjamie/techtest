c18b70d10d385dffa6ca0f1085cad6dd7d77e4eb
wrightjamie <wright_jamie@hotmail.com>
2022-07-30 23:58:48 +0100

    Tidy Imports

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "gatsby";
-import styled from "styled-components";
 
 const Card = ({}) => {
   const images = ["advTrg.jpg", "glider.jpg", "tutor.jpg"];
