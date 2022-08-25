880c4bedc937723a9d4afdd2283f1cd1f15efb72
wrightjamie <wright_jamie@hotmail.com>
2022-08-25 23:02:16 +0100

    Add focus-within to change header to fixed

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -82,8 +82,13 @@ const HeaderContainer = styled(UtilityContainer)`
   box-shadow: 0px 3px 5px 3px rgba(0, 0, 0, 0.2);
 
   color: var(--col-light);
+
+  &:focus-within {
+    position: fixed;
+  }
 `;
 
 const StyledNav = styled(UtilityFlex)`
   margin-left: auto;
+  font-size: var(--f-s-500);
 `;
