d0e6bba6eb5c31682b88bed0a2442e6b39e799fe
wrightjamie <wright_jamie@hotmail.com>
2022-08-25 23:11:48 +0100

    Add Link

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,6 +9,19 @@ import Header from "./header/header";
 import Footer from "./footer/footer";
 import { LayoutContainer, UtilityContainer } from "./utils/utility";
 
+const Skip = styled.a`
+  background: #e77e23;
+  height: 30px;
+  left: 50%;
+  padding: 8px;
+  position: absolute;
+  transform: translateY(-100%);
+  transition: transform 0.3s;
+  &:focus {
+    transform: translateY(0%);
+  }
+`;
+
 const Layout = ({ children, lead }) => {
   const data = useStaticQuery(
     graphql`
@@ -49,6 +62,7 @@ const Layout = ({ children, lead }) => {
   return (
     <>
       <GlobalStyle />
+      <Skip href="#page1">Skip to content</Skip>
       <LayoutContainer as="main">
         <Header data={data} />
         {lead}
