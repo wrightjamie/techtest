dbc2f8b2ffb2b988541873c4a479e5a74f6d6ca9
wrightjamie <wright_jamie@hotmail.com>
2022-08-25 15:57:12 +0100

    Social List Flex & No Bullet

--- src/components/utils/social_items.js
+++ src/components/utils/social_items.js
@@ -5,29 +5,33 @@ import Twitter from "../../svg/twitter.svg";
 import Phone from "../../svg/phone.svg";
 import Mail from "../../svg/mail.svg";
 
+import { UtilityNavList } from "./utility";
+
 export const SocialItems = () => {
   return (
-    <ul>
-      <li>
-        <a href="facebook.com">
-          <Facebook />
-        </a>
-      </li>
-      <li>
-        <a href="twitter.com">
-          <Twitter />
-        </a>
-      </li>
-      <li>
-        <a href="phone.com">
-          <Phone />
-        </a>
-      </li>
-      <li>
-        <a href="contact.com">
-          <Mail />
-        </a>
-      </li>
-    </ul>
+    <>
+      <UtilityNavList>
+        <li>
+          <a href="facebook.com">
+            <Facebook />
+          </a>
+        </li>
+        <li>
+          <a href="twitter.com">
+            <Twitter />
+          </a>
+        </li>
+        <li>
+          <a href="phone.com">
+            <Phone />
+          </a>
+        </li>
+        <li>
+          <a href="contact.com">
+            <Mail />
+          </a>
+        </li>
+      </UtilityNavList>
+    </>
   );
 };
