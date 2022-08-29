3646439edb57847b9cbdbd08b4be0f2ae92ed4ec
wrightjamie <wright_jamie@hotmail.com>
2022-08-29 22:11:53 +0100

    Menu Button moved out of Header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby";
 import styled from "styled-components";
 
 import { H1Link } from "./header_h1";
+import { MenuButton } from "./menu_button";
 
 import {
   UtilityContainer,
@@ -35,13 +36,7 @@ const Header = ({ data }) => {
           </NavItems>
         </StyledNav>
         <Overlay open={isMenuOpen} />
-        <MenuButtonHolder>
-          <MenuButton onClick={onClick} open={isMenuOpen}>
-            <div></div>
-            <div></div>
-            <div></div>
-          </MenuButton>
-        </MenuButtonHolder>
+        <MenuButton open={isMenuOpen} onClickCallback={onClick} />
       </HeaderWrapper>
     </HeaderContainer>
   );
@@ -95,70 +90,6 @@ const NavItems = styled(UtilityNavList)`
         color: var(--col-header);
         text-decoration: none;
       }
-    }
-  }
-`;
-
-const MenuButtonHolder = styled.div`
-  display: none;
-  margin-left: auto;
-  //margin-right: calc(2 * var(--gap));
-  position: fixed;
-  top: var(--half-gap);
-  right: calc(2.5 * var(--gap));
-  height: calc(var(--headerHeight) - var(--gap));
-  width: calc(var(--headerHeight) - var(--gap));
-  align-items: center;
-  justify-content: center;
-
-  @media (max-width: 50em) {
-    display: flex;
-  }
-`;
-
-const MenuButton = styled.button`
-  border-radius: 100%;
-  background-color: transparent;
-  margin: var(--gap);
-  border: none;
-  cursor: pointer;
-  padding: 0;
-  height: 80%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-
-  transition: all 0.3s ease;
-  &:hover,
-  &:focus {
-    background-color: var(--col-header);
-    box-shadow: 0 0 var(--half-gap) var(--half-gap) var(--col-header);
-    div {
-      opacity: 1;
-    }
-  }
-
-  div {
-    border-radius: 0.1rem;
-    width: 1.65rem;
-    height: 0.2rem;
-    background-color: var(--col-light);
-    opacity: 0.7;
-
-    transition: all 0.3s linear;
-    transform-origin: 1px;
-
-    :first-child {
-      transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
-    }
-
-    :nth-child(2) {
-      ${({ open }) => (open ? "opacity: 0;" : "")}
-      transform: ${({ open }) => (open ? "translateX(10px)" : "translateX(0)")};
-    }
-
-    :nth-child(3) {
-      transform: ${({ open }) => (open ? "rotate(-45deg)" : "rotate(0)")};
     }
   }
 `;
