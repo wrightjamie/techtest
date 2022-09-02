3c462a76520d00c0c1740c99cb323f820fe24c5b
wrightjamie <wright_jamie@hotmail.com>
2022-09-03 00:41:18 +0100

    remove post collection separate dile

--- src/cms/collections/home.js
+++ src/cms/collections/home.js
@@ -33,7 +33,15 @@ const home = {
       ],
     },
     links("Header Links", "header_links"),
-    post_collection(),
+    {
+      label: "Choose Post",
+      name: "post",
+      widget: "relation",
+      collection: "post",
+      search_fields: ["title", "excerpt"],
+      value_field: "slug",
+      display_fields: ["title"],
+    },
   ],
 };
 
