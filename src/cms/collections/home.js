299577b35b1a1510c24fcb4af697f90303e9c25c
wrightjamie <wright_jamie@hotmail.com>
2022-09-02 22:25:32 +0100

    Display FIelds to an array

--- src/cms/collections/home.js
+++ src/cms/collections/home.js
@@ -36,7 +36,7 @@ const home = {
       collection: "posts",
       search_fields: ["title", "excerpt"],
       value_field: "slug",
-      display_fields: "title",
+      display_fields: ["title"],
     },
   ],
 };
