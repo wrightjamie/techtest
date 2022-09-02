673aee71418501677679ce5153fcfcba784cbe31
wrightjamie <wright_jamie@hotmail.com>
2022-09-02 22:20:03 +0100

    First attempt at relational field

--- src/cms/collections/home.js
+++ src/cms/collections/home.js
@@ -29,7 +29,22 @@ const home = {
         },
       ],
     },
+    {
+      label: "Choose Post",
+      name: "post",
+      widget: "relation",
+      collection: "posts",
+      search_fields: ["title", "excerpt"],
+      value_field: "slug",
+      display_fields: "title",
+    },
   ],
 };
 
 export default home;
+
+const three_page = {};
+
+const four_page = {};
+
+const hero_page = {};
