6f6716985ca0d88ffd21bcaf1f322d740c8c5067
wrightjamie <wright_jamie@hotmail.com>
2022-09-04 10:02:08 +0100

    Remove post_collection duplicate from home.js

--- src/cms/collections/home.js
+++ src/cms/collections/home.js
@@ -34,15 +34,6 @@ const home = {
       ],
     },
     links("Header Links", "header_links"),
-    {
-      label: "Choose Post",
-      name: "post",
-      widget: "relation",
-      collection: "post",
-      search_fields: ["title", "excerpt"],
-      value_field: "slug",
-      display_fields: ["title"],
-    },
     post_collection,
   ],
 };
