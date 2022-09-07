885ec1568bcce1487cef33e1447d2ec01e789542
wrightjamie <wright_jamie@hotmail.com>
2022-09-07 23:40:26 +0100

    Change to function

--- src/cms/partials/pages.js
+++ src/cms/partials/pages.js
@@ -1,22 +1,23 @@
 import post_collection from "../partials/post_collection";
 
 const page_defaults = {
-  name: "posts",
   widget: "list",
   field: post_collection,
   allow_add: true,
 };
 
-export const page_of_three = {
+export const page_of_three = (name = "page3") => ({
   ...page_defaults,
+  name
   label: "Select 3 Posts",
   max: 3,
   min: 3,
-};
+});
 
-export const page_of_four = {
+export const page_of_four = (name = "page4") => ({
   ...page_defaults,
+  name,
   label: "Select 4 Posts",
   max: 4,
   min: 4,
-};
+});
