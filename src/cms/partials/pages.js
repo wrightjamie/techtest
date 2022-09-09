47fdf9ba0d53de6b318d7b3953441ddefccd5773
wrightjamie <wright_jamie@hotmail.com>
2022-09-09 22:42:12 +0100

    More advanced pages (made too many changes here!)

--- src/cms/partials/pages.js
+++ src/cms/partials/pages.js
@@ -1,36 +1,42 @@
 import post_collection from "../partials/post_collection";
 
-export const page_of_three = (name = "page3") => ({
-  label: "Page of 3",
+const page_title = {
+  label: "Page Title",
+  name: "title",
+  widget: "string",
+  required: true,
+};
+
+const page = (name, label, max, min) => ({
+  label,
   name,
   widget: "object",
   fields: [
-    {
-      label: "Page Title",
-      name: "title",
-      widget: "string",
-      required: true,
-    },
+    page_title,
     {
       widget: "list",
       field: post_collection,
       allow_add: true,
       name,
-      label: "Select 3 Posts",
-      max: 3,
-      min: 3,
+      label: "Select Posts or Pages",
+      max,
+      min,
     },
   ],
 });
 
-export const page_of_four = (name = "page4") => ({
-  widget: "list",
-  field: post_collection,
-  allow_add: true,
-  name,
-  label: "Select 4 Posts",
-  max: 4,
-  min: 4,
-});
+export const page_of_three = (name = "page3") =>
+  page(name, "Page of 3 Elements", 3, 3);
 
-export const page_of_cheques = (name = "page5") => page_of_four(name);
+export const page_of_four = (name = "page4") =>
+  page(name, "Page of 4 Elements", 4, 4);
+
+export const page_of_cheques = (name = "page5") =>
+  page(name, "Page of 4 Elements (Chequed)", 4, 4);
+
+export const page_of_recent = (name = "page2") => ({
+  label: "Recent Posts",
+  name,
+  widget: "object",
+  fields: [page_title],
+});
