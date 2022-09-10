2e1769dc72da0e4a50247ff9dcdb7631c3692e17
wrightjamie <wright_jamie@hotmail.com>
2022-09-10 09:59:03 +0100

    extract link component

--- src/cms/partials/links.js
+++ src/cms/partials/links.js
@@ -1,36 +1,36 @@
+export const link = {
+  label: "Link",
+  name: "link",
+  widget: "object",
+  fields: [
+    {
+      label: "Content",
+      name: "content",
+      widget: "string",
+      required: false,
+    },
+    {
+      label: "URL",
+      name: "url",
+      widget: "string",
+      required: false,
+    },
+    {
+      label: "Icon",
+      name: "icon",
+      widget: "select",
+      options: ["facebook", "twitter", "mail", "phone", "none"],
+    },
+  ],
+};
+
 const links = (label = "Link", name = "links", min = 1, max = 5) => ({
   label,
   name,
   widget: "list",
   min,
   max,
-  fields: [
-    {
-      label: "Link",
-      name: "link",
-      widget: "object",
-      fields: [
-        {
-          label: "Content",
-          name: "content",
-          widget: "string",
-          required: false,
-        },
-        {
-          label: "URL",
-          name: "url",
-          widget: "string",
-          required: false,
-        },
-        {
-          label: "Icon",
-          name: "icon",
-          widget: "select",
-          options: ["facebook", "twitter", "mail", "phone", "none"],
-        },
-      ],
-    },
-  ],
+  fields: [link],
 });
 
 export default links;
