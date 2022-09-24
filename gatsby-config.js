e427af2b8be56cc8eaa0b53bbb0eb688c12eaee6
wrightjamie <wright_jamie@hotmail.com>
2022-09-25 00:30:04 +0100

    Remove filesystem link that doesn't exist

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,13 +28,6 @@ module.exports = {
         name: "pages",
       },
     },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/src/img`,
-        name: "images",
-      },
-    },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
