930cb04190410e96c7211d519f4b545fb5f1fff8
wrightjamie <wright_jamie@hotmail.com>
2022-11-08 23:00:46 +0000

    add icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
+        icon: `src/svg/rafroundel.svg`,
       },
     },
 
